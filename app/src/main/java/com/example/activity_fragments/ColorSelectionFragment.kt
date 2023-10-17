a4fabd515fbd0592d58dd2dcfb73f42e9aec878e
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2023-10-17 11:59:45 -0400

    Set the spinner inside the ColorSelectionFragment

--- app/src/main/java/com/example/activity_fragments/ColorSelectionFragment.kt
+++ app/src/main/java/com/example/activity_fragments/ColorSelectionFragment.kt
@@ -1,10 +1,14 @@
 package com.example.activity_fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class ColorSelectionFragment : Fragment() {
@@ -14,6 +18,24 @@ class ColorSelectionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color_selection, container, false)
+        val layout = inflater.inflate(R.layout.fragment_color_selection, container, false)
+        val spinner = layout.findViewById<Spinner>(R.id.spinner)
+
+        spinner.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, arrayOf(
+            "Red", "White", "Black", "Cyan", "Yellow"
+        ))
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run {
+                    layout.setBackgroundColor(Color.parseColor(getItemAtPosition(p2).toString()))
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+
+        return layout
     }
 }
